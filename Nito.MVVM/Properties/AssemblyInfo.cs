92c16a31b83baeea2e3c3ebd562042148b6a4318
Stephen Cleary <StephenCleary@users.noreply.github.com>
2009-07-03 00:21:10 +0000

    Initial checkin.

---
+++ Nito.MVVM/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nito.MVVM")]
+[assembly: AssemblyDescription("MVVM helper classes")]
+[assembly: AssemblyCompany("Nito Programs")]
+[assembly: AssemblyProduct("Nito libraries")]
+[assembly: AssemblyCopyright("Copyright © 2009 Nito Programs")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("1.0.0.0")]
