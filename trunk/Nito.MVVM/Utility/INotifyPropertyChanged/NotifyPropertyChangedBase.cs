8368b7888f7ff954e75618e8cd2fbacea63ef803
shammah_cp <SND\shammah_cp>
2009-07-13 18:59:30 +0000

    Added INotifyPropertyChanged helpers.

---
+++ trunk/Nito.MVVM/Utility/INotifyPropertyChanged/NotifyPropertyChangedBase.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="NotifyPropertyChangedBase.cs" company="Nito Programs">
+//     Copyright (c) 2009 Nito Programs.
+// </copyright>
+
+namespace Nito.Utility
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// A base class for classes that need to implement <see cref="INotifyPropertyChanged"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>If deriving from this class is not possible, use <see cref="NotifyPropertyChangedImpl"/> instead.</para>
+    /// </remarks>
+    /// <typeparam name="TObject">The type of the derived class.</typeparam>
+    public abstract class NotifyPropertyChangedBase<TObject> : INotifyPropertyChanged
+        where TObject : NotifyPropertyChangedBase<TObject>
+    {
+        /// <summary>
+        /// The backing delegate for <see cref="PropertyChanged"/>.
+        /// </summary>
+        private PropertyChangedEventHandler propertyChanged;
+
+        /// <summary>
+        /// Provides notification of changes to a property value.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { this.propertyChanged += value; }
+            remove { this.propertyChanged -= value; }
+        }
+
+        /// <summary>
+        /// Raises <see cref="PropertyChanged"/> for the given property.
+        /// </summary>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <param name="expression">The lambda expression identifying the property that changed.</param>
+        protected void OnPropertyChanged<TProperty>(Expression<Func<TObject, TProperty>> expression)
+        {
+            // The cast of "this" to TObject will always succeed due to the generic constraint on this class
+            this.propertyChanged.Raise((TObject)this, expression);
+        }
+    }
+}
---
+++ trunk/UnitTests/NotifyPropertyChangedUnitTests.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nito.Utility;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class NotifyPropertyChangedUnitTests
+    {
+        [TestMethod]
+        public void TestSubscription()
+        {
+            TestObject test = new TestObject();
+            bool called = false;
+
+            test.SubscribeToPropertyChanged(x => x.ValueProperty, x => called = true);
+
+            Assert.AreEqual(false, called);
+
+            test.ObjectProperty = new object();
+
+            Assert.AreEqual(false, called);
+
+            test.ValueProperty = 13;
+
+            Assert.AreEqual(true, called);
+        }
+
+        [TestMethod]
+        public void TestDerivedSubscription()
+        {
+            TestDerivedObject test = new TestDerivedObject();
+            bool called = false;
+
+            // Ensure the PropertyChangedEventHandler.Raise extension method will not fail when its "this" parameter is null
+            test.ObjectProperty = new object();
+
+            test.SubscribeToPropertyChanged(x => x.ValueProperty, x => called = true);
+
+            Assert.AreEqual(false, called);
+
+            test.ObjectProperty = new object();
+
+            Assert.AreEqual(false, called);
+
+            test.ValueProperty = 13;
+
+            Assert.AreEqual(true, called);
+        }
+
+        [TestMethod]
+        public void TestIncludedSubscription()
+        {
+            TestIncludedObject test = new TestIncludedObject();
+            bool called = false;
+
+            // Ensure the PropertyChangedEventHandler.Raise extension method will not fail when its "this" parameter is null
+            test.ObjectProperty = new object();
+
+            test.SubscribeToPropertyChanged(x => x.ValueProperty, x => called = true);
+
+            Assert.AreEqual(false, called);
+
+            test.ObjectProperty = new object();
+
+            Assert.AreEqual(false, called);
+
+            test.ValueProperty = 13;
+
+            Assert.AreEqual(true, called);
+        }
+
+        /// <summary>
+        /// Test object that has its own INotifyPropertyChanged implementation (used to test SubscribeToPropertyChanged)
+        /// </summary>
+        private sealed class TestObject : INotifyPropertyChanged
+        {
+            private int valueProperty;
+            private object objectProperty;
+
+            public int ValueProperty
+            {
+                get { return this.valueProperty; }
+                set
+                {
+                    this.valueProperty = value;
+                    this.OnPropertyChanged(this.GetPropertyName(x => x.ValueProperty));
+                }
+            }
+
+            public object ObjectProperty
+            {
+                get { return this.objectProperty; }
+                set
+                {
+                    this.objectProperty = value;
+                    this.OnPropertyChanged(this.GetPropertyName(x => x.ObjectProperty));
+                }
+            }
+
+            #region INotifyPropertyChanged Members
+
+            public void OnPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Test object that uses an inherited implementation of INotifyPropertyChanged.
+        /// </summary>
+        private sealed class TestDerivedObject : NotifyPropertyChangedBase<TestDerivedObject>
+        {
+            private int valueProperty;
+            private object objectProperty;
+
+            public int ValueProperty
+            {
+                get { return this.valueProperty; }
+                set
+                {
+                    this.valueProperty = value;
+                    this.OnPropertyChanged(x => x.ValueProperty);
+                }
+            }
+
+            public object ObjectProperty
+            {
+                get { return this.objectProperty; }
+                set
+                {
+                    this.objectProperty = value;
+                    this.OnPropertyChanged(x => x.ObjectProperty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Test object that uses the contained implementation of INotifyPropertyChanged.
+        /// </summary>
+        private sealed class TestIncludedObject : INotifyPropertyChanged
+        {
+            private NotifyPropertyChangedImpl<TestIncludedObject> propertyChanged;
+            private int valueProperty;
+            private object objectProperty;
+
+            public TestIncludedObject()
+            {
+                this.propertyChanged = new NotifyPropertyChangedImpl<TestIncludedObject>(this);
+            }
+
+            public int ValueProperty
+            {
+                get { return this.valueProperty; }
+                set
+                {
+                    this.valueProperty = value;
+                    this.propertyChanged.OnPropertyChanged(x => x.ValueProperty);
+                }
+            }
+
+            public object ObjectProperty
+            {
+                get { return this.objectProperty; }
+                set
+                {
+                    this.objectProperty = value;
+                    this.propertyChanged.OnPropertyChanged(x => x.ObjectProperty);
+                }
+            }
+
+            #region INotifyPropertyChanged Members
+
+            public event PropertyChangedEventHandler PropertyChanged
+            {
+                add { this.propertyChanged.PropertyChanged += value; }
+                remove { this.propertyChanged.PropertyChanged -= value; }
+            }
+
+            #endregion
+        }
+    }
+}
