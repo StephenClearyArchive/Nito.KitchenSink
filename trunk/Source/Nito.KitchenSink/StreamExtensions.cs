ed46cfe3badd2c696234b1973abe885a4203e12c
shammah_cp <SND\shammah_cp>
2010-03-18 15:00:50 +0000

    Added a cancelable Stream.CopyTo.

--- trunk/Source/Nito.KitchenSink/StreamExtensions.cs
+++ trunk/Source/Nito.KitchenSink/StreamExtensions.cs
@@ -6,6 +6,7 @@ namespace Nito.KitchenSink
 {
     using System;
     using System.IO;
+using System.Threading;
 
     /// <summary>
     /// Provides methods useful when dealing with streams.
@@ -38,6 +39,19 @@ namespace Nito.KitchenSink
         /// <param name="progress">A callback method invoked with the number of bytes transferred so far. May be null.</param>
         public static void CopyTo(this Stream source, Stream destination, byte[] buffer, Action<long> progress)
         {
+            CopyTo(source, destination, buffer, progress, null);
+        }
+
+        /// <summary>
+        /// Synchronously copies the contents of this stream into another stream, enabling cancellation.
+        /// </summary>
+        /// <param name="source">The stream that is the source of the copy.</param>
+        /// <param name="destination">The stream that is the destination of the copy.</param>
+        /// <param name="buffer">The buffer used by the copy. The size of this buffer determines the sizes of reads and writes made to the streams.</param>
+        /// <param name="progress">A callback method invoked with the number of bytes transferred so far. May be null.</param>
+        /// <param name="cancellationToken">A cancellation token which may be used to cancel the stream copy. May be null.</param>
+        public static void CopyTo(this Stream source, Stream destination, byte[] buffer, Action<long> progress, CancellationToken? cancellationToken)
+        {
             long bytesTransferred = 0;
             while (true)
             {
@@ -52,6 +66,11 @@ namespace Nito.KitchenSink
                 {
                     bytesTransferred += bytesRead;
                     progress(bytesTransferred);
+                }
+
+                if (cancellationToken != null)
+                {
+                    cancellationToken.Value.ThrowIfCancellationRequested();
                 }
             }
         }
