8c6a27409a02dbab27c975f72fd7e56b22dc6fa8
shammah_cp <SND\shammah_cp>
2010-03-19 00:40:16 +0000

    Added ReferenceCountedDisposable and Func<TResult>.Async.

---
+++ trunk/Source/Nito.KitchenSink/DisposableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="IDisposable"/> types.
+    /// </summary>
+    public static class DisposableExtensions
+    {
+        /// <summary>
+        /// Wraps this disposable object in a reference-counted disposable wrapper.
+        /// </summary>
+        /// <typeparam name="T">The type of the underlying disposable object.</typeparam>
+        /// <param name="disposable">The underlying disposable object to wrap.</param>
+        /// <returns>A reference-counted disposable wrapper.</returns>
+        public static ReferenceCountedDisposable<T> ReferenceCounted<T>(this T disposable) where T : IDisposable
+        {
+            return new ReferenceCountedDisposable<T>(disposable);
+        }
+    }
+}
