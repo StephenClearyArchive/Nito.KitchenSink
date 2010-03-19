592151c3c3bcc3f4e6275e96e8bb6a63539a81a2
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-03-19 00:40:16 +0000

    Added ReferenceCountedDisposable and Func<TResult>.Async.

---
+++ Source/Nito.KitchenSink/ReferenceCountedDisposable.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Disposables;
+
+namespace Nito.KitchenSink
+{
+    /// <summary>
+    /// Provides a reference count for a wrapped disposable object.
+    /// </summary>
+    /// <typeparam name="T">The type of object to wrap.</typeparam>
+    public sealed class ReferenceCountedDisposable<T> : IDisposable where T : IDisposable
+    {
+        /// <summary>
+        /// The underlying reference counted disposable.
+        /// </summary>
+        private readonly RefCountDisposable disposable;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReferenceCountedDisposable&lt;T&gt;"/> class, wrapping the specified disposable object.
+        /// </summary>
+        /// <param name="value">The disposable object to wrap.</param>
+        public ReferenceCountedDisposable(T value)
+        {
+            this.disposable = new RefCountDisposable(value);
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the underlying disposable object.
+        /// </summary>
+        public T Value { get; private set; }
+
+        /// <summary>
+        /// Reduces the reference count on the underlying disposable object.
+        /// </summary>
+        void IDisposable.Dispose()
+        {
+            this.disposable.Dispose();
+        }
+
+        /// <summary>
+        /// Adds a reference count to this wrapper.
+        /// </summary>
+        /// <returns>A reference-counted wrapper around the same disposable object.</returns>
+        public Reference AddReference()
+        {
+            return new Reference(this);
+        }
+
+        /// <summary>
+        /// A reference-counted wrapper around a disposable object.
+        /// </summary>
+        public sealed class Reference : IDisposable
+        {
+            /// <summary>
+            /// A reference-counted child disposable object.
+            /// </summary>
+            private readonly IDisposable disposable;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="ReferenceCountedDisposable&lt;T&gt;.Reference"/> class with the specified parent.
+            /// </summary>
+            /// <param name="parent">The parent, which shares this reference count.</param>
+            public Reference(ReferenceCountedDisposable<T> parent)
+            {
+                this.disposable = parent.disposable.GetDisposable();
+                this.Value = parent.Value;
+            }
+
+            /// <summary>
+            /// Gets the underlying disposable object.
+            /// </summary>
+            public T Value { get; private set; }
+
+            /// <summary>
+            /// Reduces the reference count on the underlying disposable object.
+            /// </summary>
+            void IDisposable.Dispose()
+            {
+                this.disposable.Dispose();
+            }
+        }
+    }
+}
---
+++ Source/Nito.KitchenSink/TaskExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Nito.KitchenSink
+{
+    /// <summary>
+    /// Extension methods for the <see cref="Task"/> class, and task-related functionality.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Returns a delegate that invokes the original delegate asynchronously (on a ThreadPool thread), applying a timeout and supporting cancellation. If timeout or cancellation occurs, the delegate runs to completion but its results and any exceptions it raises are ignored.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result of the delegate.</typeparam>
+        /// <param name="function">The delegate to invoke asynchronously.</param>
+        /// <param name="millisecondsTimeout">The number of milliseconds to wait, or <see cref="Timeout.Infinite"/> to wait indefinitely.</param>
+        /// <param name="cancellationToken">The cancellation token to observe while waiting.</param>
+        /// <returns>A delegate that invokes the original delegate asynchronously with a timeout and supporting cancellation.</returns>
+        public static Func<TResult> Async<TResult>(this Func<TResult> function, int millisecondsTimeout, CancellationToken cancellationToken)
+        {
+            return () =>
+            {
+                using (var signalOuter = new ManualResetEvent(false).ReferenceCounted())
+                {
+                    TResult result = default(TResult);
+                    Exception error = null;
+                    var signalInner = signalOuter.AddReference();
+                    IAsyncResult asyncResult = function.BeginInvoke(
+                        innerResult =>
+                        {
+                            try
+                            {
+                                result = function.EndInvoke(innerResult);
+                            }
+                            catch (Exception ex)
+                            {
+                                error = ex;
+                            }
+
+                            signalInner.Value.Set();
+                            ((IDisposable)signalInner).Dispose();
+                        },
+                        null);
+
+                    if (asyncResult.AsyncWaitHandle.WaitOne(millisecondsTimeout, cancellationToken))
+                    {
+                        signalOuter.Value.WaitOne();
+                        if (error != null)
+                        {
+                            throw error.PrepareForRethrow();
+                        }
+
+                        return result;
+                    }
+                    else
+                    {
+                        throw new TimeoutException("Task did not complete in a timely manner.");
+                    }
+                }
+            };
+        }
+
+        /// <summary>
+        /// Waits for a handle to be signalled, allowing cancellation.
+        /// </summary>
+        /// <param name="waitHandle">The wait handle to observe.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <exception cref="OperationCanceledException">The cancellation token was signalled before the operation completed.</exception>
+        public static void WaitOne(this WaitHandle waitHandle, CancellationToken cancellationToken)
+        {
+            waitHandle.WaitOne(Timeout.Infinite, cancellationToken);
+        }
+
+        /// <summary>
+        /// Waits for a handle to be signalled for a specified time, allowing cancellation. Returns <c>true</c> if the handle was signalled, and <c>false</c> if there was a timeout.
+        /// </summary>
+        /// <param name="waitHandle">The wait handle to observe.</param>
+        /// <param name="millisecondsTimeout">The amount of time to wait for the handle to be signalled, in milliseconds; or <see cref="Timeout.Infinite"/> for an infinite wait.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <exception cref="OperationCanceledException">The cancellation token was signalled before the operation completed.</exception>
+        /// <returns>Returns <c>true</c> if the handle was signalled, and <c>false</c> if there was a timeout.</returns>
+        public static bool WaitOne(this WaitHandle waitHandle, int millisecondsTimeout, CancellationToken cancellationToken)
+        {
+            var waitFor = new WaitHandle[] { waitHandle, cancellationToken.WaitHandle };
+            int result = WaitHandle.WaitAny(waitFor, millisecondsTimeout);
+            return result != WaitHandle.WaitTimeout;
+        }
+    }
+}
