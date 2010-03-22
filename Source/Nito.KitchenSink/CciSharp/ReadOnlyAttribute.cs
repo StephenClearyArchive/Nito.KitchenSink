916aa150a3e1ee43e102dba93708622379b78359
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-03-22 14:56:30 +0000

    Added CciSharp attributes.

---
+++ Source/Nito.KitchenSink/CciSharp/ReadOnlyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink
+{
+    /// <summary>
+    /// When used with CciSharp, makes an auto-property read-only. This can only be applied to non-virtual instance properties. The setter on the property must be private and only called from the constructor.
+    /// See http://ccisamples.codeplex.com/wikipage?title=CciSharp.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public sealed class ReadOnlyAttribute : Attribute
+    {
+    }
+}
