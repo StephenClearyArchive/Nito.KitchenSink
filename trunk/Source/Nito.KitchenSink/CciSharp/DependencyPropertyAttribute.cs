1eecce76812263b073b9d68d012b30854c722bf0
shammah_cp <SND\shammah_cp>
2010-03-22 14:56:30 +0000

    Added CciSharp attributes.

---
+++ trunk/Source/Nito.KitchenSink/CciSharp/DependencyPropertyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink
+{
+    /// <summary>
+    /// When used with CciSharp, turns an auto-property into a property that supports DependencyProperty. This can only be applied to non-virtual instance properties.
+    /// See http://ccisamples.codeplex.com/wikipage?title=CciSharp.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public sealed class DependencyPropertyAttribute : Attribute
+    {
+    }
+}
