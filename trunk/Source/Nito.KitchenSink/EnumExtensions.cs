2f3cd8c8a82571251928a7ac48ebc85ce73e4a1e
shammah_cp <SND\shammah_cp>
2010-04-06 18:56:13 +0000

    Added Enum extensions (for bitfields).

---
+++ trunk/Source/Nito.KitchenSink/EnumExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink
+{
+    /// <summary>
+    /// Provides useful extensions for enumerations.
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Determines if a bit flag or set of bit flags are set in an enumeration value.
+        /// </summary>
+        /// <typeparam name="T">The type of enumeration.</typeparam>
+        /// <param name="value">The value to test for the bit flag(s).</param>
+        /// <param name="flag">The flag(s) to test for.</param>
+        /// <returns><c>true</c> if <paramref name="value"/> contains the bit flags defined in <paramref name="flag"/>; otherwise, <c>false</c>.</returns>
+        public static bool Contains<T>(this Enum value, T flag)
+        {
+            try
+            {
+                return (Convert.ToInt64(value) & Convert.ToInt64(flag)) != 0;
+            }
+            catch (OverflowException)
+            {
+                return (Convert.ToUInt64(value) & Convert.ToUInt64(flag)) != 0;
+            }
+        }
+
+        /// <summary>
+        /// Combines an enumeration value with a bit flag or set of bit flags and returns the new value.
+        /// </summary>
+        /// <typeparam name="T">The type of enumeration.</typeparam>
+        /// <param name="value">The value to combine with the bit flag(s).</param>
+        /// <param name="flag">The flag(s) to add to <paramref name="value"/>.</param>
+        /// <returns><paramref name="value"/> combined with <paramref name="flag"/>.</returns>
+        public static T Add<T>(this Enum value, T flag)
+        {
+            try
+            {
+                return (T)Convert.ChangeType(Convert.ToInt64(value) | Convert.ToInt64(flag), Enum.GetUnderlyingType(typeof(T)));
+            }
+            catch (OverflowException)
+            {
+                return (T)Convert.ChangeType(Convert.ToUInt64(value) | Convert.ToUInt64(flag), Enum.GetUnderlyingType(typeof(T)));
+            }
+        }
+
+        /// <summary>
+        /// Removes a bit flag of set of bit flags from an enumeration value and returns the new value.
+        /// </summary>
+        /// <typeparam name="T">The type of enumeration.</typeparam>
+        /// <param name="value">The value from which to remove the bit flag(s).</param>
+        /// <param name="flag">The flag(s) to remove from <paramref name="value"/>.</param>
+        /// <returns><paramref name="value"/> with any flags in <paramref name="flag"/> removed.</returns>
+        public static T Remove<T>(this Enum value, T flag)
+        {
+            try
+            {
+                return (T)Convert.ChangeType(Convert.ToInt64(value) & ~Convert.ToInt64(flag), Enum.GetUnderlyingType(typeof(T)));
+            }
+            catch (OverflowException)
+            {
+                return (T)Convert.ChangeType(Convert.ToUInt64(value) & ~Convert.ToUInt64(flag), Enum.GetUnderlyingType(typeof(T)));
+            }
+        }
+    }
+}
