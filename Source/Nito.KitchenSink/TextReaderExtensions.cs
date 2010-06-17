59ba3a27dd1b93f7bcf54bbe7a3a2572addd029d
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-06-17 19:38:08 +0000

    Added basic (and not completely tested) RFC4180 CSV readers and writers.

---
+++ Source/Nito.KitchenSink/TextReaderExtensions.cs
@@ -0,0 +1,60 @@
+﻿namespace Nito.KitchenSink
+{
+    using System.IO;
+
+    /// <summary>
+    /// Extension methods for <see cref="TextReader"/> classes.
+    /// </summary>
+    public static class TextReaderExtensions
+    {
+        /// <summary>
+        /// Wraps this <see cref="TextReader"/> with a putback buffer.
+        /// </summary>
+        /// <param name="source">The <see cref="TextReader"/> to wrap.</param>
+        /// <returns>A <see cref="TextReader"/> with a putback buffer.</returns>
+        public static PutbackBufferTextReader<T> ApplyPutbackBuffer<T>(this T source) where T : TextReader
+        {
+            return new PutbackBufferTextReader<T>(source);
+        }
+
+        /// <summary>
+        /// Wraps this <see cref="TextReader"/>, converting any <c>\r\n</c>, <c>\r</c>, or <c>\n</c> character sequences to just <c>\n</c>.
+        /// </summary>
+        /// <param name="source">The <see cref="TextReader"/> to wrap.</param>
+        /// <returns>A <see cref="TextReader"/> with normalized line endings.</returns>
+        public static NormalizeCrLfTextReader<T> NormalizeCrLf<T>(this T source) where T : TextReader
+        {
+            return new NormalizeCrLfTextReader<T>(source);
+        }
+
+        /// <summary>
+        /// Wraps this <see cref="TextReader"/>, counting the lines and columns as characters are read.
+        /// </summary>
+        /// <param name="source">The <see cref="TextReader"/> to wrap.</param>
+        /// <returns>A <see cref="TextReader"/> with counted lines and columns.</returns>
+        public static LineMappingTextReader<T> ApplyLineMapping<T>(this T source) where T : TextReader
+        {
+            return new LineMappingTextReader<T>(source);
+        }
+
+        /// <summary>
+        /// Wraps this <see cref="TextReader"/>, ensuring that <see cref="TextReader.Peek"/> will only return <c>-1</c> when the end of the stream is reached.
+        /// </summary>
+        /// <param name="source">The <see cref="TextReader"/> to wrap.</param>
+        /// <returns>A peekable <see cref="TextReader"/>.</returns>
+        public static PeekableTextReader<T> Peekable<T>(this T source) where T : TextReader
+        {
+            return new PeekableTextReader<T>(source);
+        }
+
+        /// <summary>
+        /// Wraps this <see cref="TextReader"/>, tracking the position of the stream as it is read.
+        /// </summary>
+        /// <param name="source">The <see cref="TextReader"/> to wrap.</param>
+        /// <returns>A <see cref="TextReader"/> with position tracking.</returns>
+        public static PositionTrackingTextReader<T> ApplyPositionTracking<T>(this T source) where T : TextReader
+        {
+            return new PositionTrackingTextReader<T>(source);
+        }
+    }
+}
---
+++ Source/Nito.KitchenSink/TextReaders/LineMappingTextReader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Nito.KitchenSink
+{
+    /// <summary>
+    /// A wrapper around a source <see cref="TextReader"/>, counting lines and columns. This class does not perform any buffering.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="TextReader"/> being wrapped.</typeparam>
+    public sealed class LineMappingTextReader<T> : SourceTextReader<PositionTrackingTextReader<T>> where T : TextReader
+    {
+        /// <summary>
+        /// The offset of each '\n' line ending seen in the source stream.
+        /// </summary>
+        private List<long> lineEndings = new List<long>() { -1 };
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LineMappingTextReader{T}"/> class, wrapping the specified <see cref="TextReader"/>.
+        /// </summary>
+        /// <param name="source">The source <see cref="TextReader"/> to wrap.</param>
+        public LineMappingTextReader(T source)
+            : base(source.ApplyPositionTracking())
+        {
+        }
+
+        /// <summary>
+        /// Gets the line number and line offset for a speicified position in the stream.
+        /// </summary>
+        /// <param name="position">The position in the stream for which to get the line number and line offset. This should not be negative or larger than the number of characters read.</param>
+        /// <returns>The line number and line offset for the specified position in the stream.</returns>
+        public Tuple<int, long> GetLineNumberAndLineOffset(long position)
+        {
+            int index = this.lineEndings.BinarySearch(position);
+            if (index > 0)
+            {
+                return Tuple.Create(index - 1, (long)0);
+            }
+
+            int lineNumber = ~index - 1;
+            return Tuple.Create(lineNumber, position - this.lineEndings[lineNumber]);
+        }
+
+        /// <summary>
+        /// Reads the next character from the input stream and advances the character position by one character.
+        /// </summary>
+        /// <returns>The next character from the input stream, or -1 if no more characters are available.</returns>
+        /// <exception cref="T:System.ObjectDisposedException">The <see cref="T:System.IO.TextReader"/> is closed. </exception>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+        public override int Read()
+        {
+            int ret = base.Read();
+
+            if (ret == -1)
+            {
+                return ret;
+            }
+
+            if (ret == '\n')
+            {
+                this.lineEndings.Add(this.BaseReader.Position);
+            }
+
+            return ret;
+        }
+    }
+}
