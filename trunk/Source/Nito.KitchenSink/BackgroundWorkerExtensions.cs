2978ffcf547a6b317db7ba3a133fbc6ae379e36c
shammah_cp <SND\shammah_cp>
2010-06-18 14:53:57 +0000

    Added ProgressReporter and BackgroundWorker.ToTask.

---
+++ trunk/Source/Nito.KitchenSink/BackgroundWorkerExtensions.cs
@@ -0,0 +1,75 @@
+﻿namespace Nito.KitchenSink
+{
+    using System;
+    using System.ComponentModel;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Provides extension methods for the <see cref="BackgroundWorker"/> class.
+    /// </summary>
+    public static class BackgroundWorkerExtensions
+    {
+        /// <summary>
+        /// Provides a <see cref="Task"/> interface for <see cref="BackgroundWorker"/>. This wrapper supports successful results, exceptions, and BGW-style cancellation.
+        /// </summary>
+        /// <param name="this">The <see cref="BackgroundWorker"/> to wrap.</param>
+        /// <returns>A <see cref="Task"/> interface for the <see cref="BackgroundWorker"/>.</returns>
+        public static Task<object> ToTask(this BackgroundWorker @this)
+        {
+            var control = new TaskCompletionSource<object>(TaskCreationOptions.LongRunning);
+            @this.RunWorkerCompleted += (sender, args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        control.SetException(args.Error);
+                    }
+                    else if (args.Cancelled)
+                    {
+                        control.SetCanceled();
+                    }
+                    else
+                    {
+                        control.SetResult(args.Result);
+                    }
+                };
+            return control.Task;
+        }
+
+        /// <summary>
+        /// Provides a <see cref="Task"/> interface for <see cref="BackgroundWorker"/>. This wrapper supports successful results, exceptions, BGW-style cancellation, and Task-style cancellation.
+        /// </summary>
+        /// <param name="this">The <see cref="BackgroundWorker"/> to wrap.</param>
+        /// <param name="cancellationToken">The cancellation token used for Task-style cancellation. When this token is signalled, <see cref="BackgroundWorker.CancelAsync"/> is invoked, and the BGW may use BGW-style or Task-style cancellation.</param>
+        /// <returns>A <see cref="Task"/> interface for the <see cref="BackgroundWorker"/>.</returns>
+        public static Task<object> ToTask(this BackgroundWorker @this, CancellationToken cancellationToken)
+        {
+            cancellationToken.Register(@this.CancelAsync);
+            var control = new TaskCompletionSource<object>(TaskCreationOptions.LongRunning);
+            @this.RunWorkerCompleted += (sender, args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        var oce = args.Error as OperationCanceledException;
+                        if (oce != null && oce.CancellationToken == cancellationToken)
+                        {
+                            control.SetCanceled();
+                        }
+                        else
+                        {
+                            control.SetException(args.Error);
+                        }
+                    }
+                    else if (args.Cancelled)
+                    {
+                        control.SetCanceled();
+                    }
+                    else
+                    {
+                        control.SetResult(args.Result);
+                    }
+                };
+            return control.Task;
+        }
+    }
+}
---
+++ trunk/Source/Nito.KitchenSink/ProgressReporter.cs
@@ -0,0 +1,44 @@
+﻿namespace Nito.KitchenSink
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A class used by Tasks to report progress or completion updates back to the UI.
+    /// </summary>
+    public sealed class ProgressReporter
+    {
+        /// <summary>
+        /// The underlying scheduler for the UI's synchronization context.
+        /// </summary>
+        private readonly TaskScheduler scheduler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressReporter"/> class. This should be run on a UI thread.
+        /// </summary>
+        public ProgressReporter()
+        {
+            this.scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        }
+
+        /// <summary>
+        /// Reports the progress to the UI thread. Note that the progress update is asynchronous with respect to the reporting Task. For a synchronous progress update, wait on the returned <see cref="Task"/>.
+        /// </summary>
+        /// <param name="action">The action to perform in the context of the UI thread. Note that this action is run asynchronously on the UI thread.</param>
+        /// <returns>The task queued to the UI thread.</returns>
+        public Task ReportProgressAsync(Action action)
+        {
+            return Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.None, this.scheduler);
+        }
+
+        /// <summary>
+        /// Reports the progress to the UI thread, and waits for the UI thread to process the update before returning.
+        /// </summary>
+        /// <param name="action">The action to perform in the context of the UI thread.</param>
+        public void ReportProgress(Action action)
+        {
+            this.ReportProgressAsync(action).Wait();
+        }
+    }
+}
