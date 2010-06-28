03f9d0c92075a35625787197795a102a36661968
shammah_cp <SND\shammah_cp>
2010-06-28 13:36:33 +0000

    Partial DelimitedText work.

---
+++ trunk/Source/Nito.KitchenSink/DelimitedText/Lexer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink.DelimitedText
+{
+    /// <summary>
+    /// Parses an input stream into tokens.
+    /// </summary>
+    public sealed class Lexer
+    {
+    }
+}
