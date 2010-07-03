e9b3fb242d5d5c8b45fa896ba77805efb3f09975
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-07-03 15:57:10 +0000

    Added extensible objects, similar to WCF's extensible objects except that these extensions can be applied to any type of extensible object.

---
+++ Source/Nito.KitchenSink/ExtensibleObjects/ExtensibleObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink.ExtensibleObjects
+{
+    /// <summary>
+    /// Combines an object with a collection of extensions.
+    /// </summary>
+    /// <typeparam name="T">The type of object to wrap.</typeparam>
+    public sealed class ExtensibleWrapper<T> : IExtensibleObject
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExtensibleWrapper&lt;T&gt;"/> class with a default value for <see cref="Object"/>.
+        /// </summary>
+        public ExtensibleWrapper()
+        {
+            this.Extensions = new ExtensionCollection(this);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExtensibleWrapper&lt;T&gt;"/> class with the specified value for <see cref="Object"/>.
+        /// </summary>
+        /// <param name="object">The object to wrap.</param>
+        public ExtensibleWrapper(T @object)
+            : this()
+        {
+            this.Object = @object;
+        }
+
+        /// <summary>
+        /// Gets or sets the object that is made extensible by this wrapper.
+        /// </summary>
+        public T Object { get; set; }
+
+        /// <summary>
+        /// Gets or the extensions applied to this object.
+        /// </summary>
+        public IExtensionCollection Extensions { get; private set; }
+    }
+}
