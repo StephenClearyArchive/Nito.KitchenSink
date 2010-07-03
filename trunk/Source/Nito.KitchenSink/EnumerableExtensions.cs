513fb6a1acc321f975cb577f1db704720e9bb1a7
shammah_cp <SND\shammah_cp>
2010-07-03 15:56:06 +0000

    Added TryGetCount.

--- trunk/Source/Nito.KitchenSink/EnumerableExtensions.cs
+++ trunk/Source/Nito.KitchenSink/EnumerableExtensions.cs
@@ -20,5 +20,32 @@ namespace Nito.KitchenSink
         {
             return new EnumeratorWrapper<T>(source);
         }
+
+        /// <summary>
+        /// Attempts to determine the count of an enumeration without enumerating it. Returns <c>true</c> if the count was successfully determined.
+        /// </summary>
+        /// <typeparam name="T">The type of objects in the enumeration.</typeparam>
+        /// <param name="source">The source enumeration.</param>
+        /// <param name="result">On return, contains the count of the enumeration if the return value was <c>true</c>. If the return value was <c>false</c>, this value is undefined.</param>
+        /// <returns><c>true</c> if the count was successfully determined; otherwise, <c>false</c>.</returns>
+        public static bool TryGetCount<T>(this IEnumerable<T> source, out int result)
+        {
+            var collection1 = source as ICollection<T>;
+            if (collection1 != null)
+            {
+                result = collection1.Count;
+                return true;
+            }
+
+            var collection2 = source as System.Collections.ICollection;
+            if (collection2 != null)
+            {
+                result = collection2.Count;
+                return true;
+            }
+
+            result = -1;
+            return false;
+        }
     }
 }
