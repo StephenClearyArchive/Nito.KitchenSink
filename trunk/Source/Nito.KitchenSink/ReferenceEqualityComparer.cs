d250ca329b9c732d949b251accb68efca04a65ff
shammah_cp <SND\shammah_cp>
2010-07-03 19:12:06 +0000

    Added ReferenceEqualityComparer.

---
+++ trunk/Source/Nito.KitchenSink/ReferenceEqualityComparer.cs
@@ -0,0 +1,43 @@
+﻿namespace Nito.KitchenSink
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An object comparer that always compares objects based on reference equality.
+    /// </summary>
+    /// <typeparam name="T">The type of objects being compared.</typeparam>
+    public sealed class ReferenceEqualityComparer<T> : IEqualityComparer<T>, IEqualityComparer where T : class
+    {
+        bool IEqualityComparer<T>.Equals(T x, T y)
+        {
+            return object.ReferenceEquals(x, y);
+        }
+
+        int IEqualityComparer<T>.GetHashCode(T obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException("obj", "Object cannot be null.");
+            }
+
+            return obj.GetHashCode();
+        }
+
+        bool IEqualityComparer.Equals(object x, object y)
+        {
+            return object.ReferenceEquals(x, y);
+        }
+
+        int IEqualityComparer.GetHashCode(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException("obj", "Object cannot be null.");
+            }
+
+            return obj.GetHashCode();
+        }
+    }
+}
