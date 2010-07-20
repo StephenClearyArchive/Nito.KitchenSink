0f736e23599c391752248108b4257ce640ba1bff
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-07-20 18:43:48 +0000

    Added SafeDisposable.

---
+++ Source/Nito.KitchenSink/Disposables/DisposableExtensions.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="DisposableExtensions.cs" company="Nito Programs">
+//     Copyright (c) 2010 Nito Programs.
+// </copyright>
+
+namespace Nito.KitchenSink
+{
+    using System;
+
+    /// <summary>
+    /// Provides extension methods for <see cref="IDisposable"/> types.
+    /// </summary>
+    public static class DisposableExtensions
+    {
+        /// <summary>
+        /// Wraps this disposable object in a reference-counted disposable wrapper.
+        /// </summary>
+        /// <typeparam name="T">The type of the underlying disposable object.</typeparam>
+        /// <param name="disposable">The underlying disposable object to wrap.</param>
+        /// <returns>A reference-counted disposable wrapper.</returns>
+        public static ReferenceCountedDisposable<T> ReferenceCounted<T>(this T disposable) where T : IDisposable
+        {
+            return new ReferenceCountedDisposable<T>(disposable);
+        }
+
+        /// <summary>
+        /// Wraps this disposable object in a wrapper which swallows any exceptions thrown from <see cref="IDisposable.Dispose"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the underlying disposable object.</typeparam>
+        /// <param name="disposable">The underlying disposable object to wrap.</param>
+        /// <returns>A safe disposable wrapper.</returns>
+        public static SafeDisposable<T> IgnoreDisposeExceptions<T>(this T disposable) where T : IDisposable
+        {
+            return new SafeDisposable<T>(disposable);
+        }
+    }
+}
---
+++ Source/Nito.KitchenSink/Disposables/SafeDisposable.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="SafeDisposable.cs" company="Nito Programs">
+//     Copyright (c) 2010 Nito Programs.
+// </copyright>
+
+namespace Nito.KitchenSink
+{
+    using System;
+
+    /// <summary>
+    /// Provides a non-throwing implementation of <see cref="IDisposable.Dispose"/> for a wrapped disposable object.
+    /// </summary>
+    /// <typeparam name="T">The type of object to wrap.</typeparam>
+    public sealed class SafeDisposable<T> : IDisposable where T : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SafeDisposable&lt;T&gt;"/> class, wrapping the specified disposable object.
+        /// </summary>
+        /// <param name="value">The disposable object to wrap.</param>
+        public SafeDisposable(T value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the underlying disposable object.
+        /// </summary>
+        public T Value { get; private set; }
+
+        /// <summary>
+        /// Disposes the underlying disposable object, swallowing all exceptions.
+        /// </summary>
+        void IDisposable.Dispose()
+        {
+            try
+            {
+                this.Value.Dispose();
+            }
+// ReSharper disable EmptyGeneralCatchClause
+            catch
+// ReSharper restore EmptyGeneralCatchClause
+            {
+                // Normally, an empty catch block is a Really Bad Idea.
+                // In this case, it's necessary to work around broken code from Microsoft (WCF client proxies, in particular).
+            }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String"/> that represents this instance.</returns>
+        public override string ToString()
+        {
+            return Convert.ToString(this.Value);
+        }
+    }
+}
