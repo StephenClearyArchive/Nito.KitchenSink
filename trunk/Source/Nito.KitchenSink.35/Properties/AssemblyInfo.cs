6be902db2feedad70ad552635742d5e1b9dda8b5
shammah_cp <SND\shammah_cp>
2010-07-23 20:27:56 +0000

    Removed .NET 3.5 project.

--- trunk/Source/Nito.KitchenSink.35/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// <copyright file="AssemblyInfo.cs" company="Nito Programs">
-//     Copyright (c) 2009-2010 Nito Programs.
-// </copyright>
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("Nito.KitchenSink for .NET 3.5 SP1 Client with Rx")]
--- trunk/Source/Source-index PDBs.cmd
+++ trunk/Source/Source-index PDBs.cmd
@@ -1,5 +1,4 @@
 @echo off
 REM This file requires Debugging Tools for Windows, Perl (e.g., ActivePerl), and svn.exe (e.g., CollabNet/Tigris)
-call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\35.Rx /Debug
 call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\40.Rx /Debug
 pause
