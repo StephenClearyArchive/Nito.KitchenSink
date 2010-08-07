b1a4a3d374f95760ef7e3afc3465d99fdab7cdd2
shammah_cp <SND\shammah_cp>
2010-08-07 14:09:54 +0000

    Moved DelimitedText under CharacterStreams, and changed to use the new Token objects.

---
+++ trunk/Source/Nito.KitchenSink/CharacterStreams/StringToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink.CharacterStreams
+{
+    /// <summary>
+    /// A string token, representing some string data.
+    /// </summary>
+    public class StringToken : Token
+    {
+        /// <summary>
+        /// The data for this token.
+        /// </summary>
+        public string Data { get; set; }
+    }
+}
--- trunk/Source/Tests/Unit Tests/EncodingObservablesUnitTests.cs
+++ trunk/Source/Tests/Unit Tests/EncodingObservablesUnitTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Concurrency;
-using Nito.KitchenSink;
+using Nito.KitchenSink.CharacterStreams;
 
 namespace Tests.Unit_Tests
 {
