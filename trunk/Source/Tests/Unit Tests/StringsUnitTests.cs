3d710bd00b616c188f4ea1933675407f36a3b3a7
shammah_cp <SND\shammah_cp>
2010-08-08 01:41:31 +0000

    Added string algorithm (edit distance).

---
+++ trunk/Source/Tests/Unit Tests/StringsUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nito.KitchenSink.Text;
+
+namespace Tests.Unit_Tests
+{
+    [TestClass]
+    public class StringsUnitTests
+    {
+        [TestMethod]
+        public void DamerauLevenshteinKnownValues()
+        {
+            Assert.AreEqual(0, Strings.DamerauLevenshteinDistance("test", "test"));
+            Assert.AreEqual(1, Strings.DamerauLevenshteinDistance("to", "ot"));
+            Assert.AreEqual(1, Strings.DamerauLevenshteinDistance("ot", "ost"));
+            Assert.AreEqual(3, Strings.DamerauLevenshteinDistance("to", "ost"));
+        }
+    }
+}
