b6fd2e7d7f0384fb9e3c0523b29d3d7a3fbfdc5f
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-09-09 20:18:04 +0000

    Changed PDB source indexer script to use new build location (under the bin directory).

--- Source/Source-index PDBs.cmd
+++ Source/Source-index PDBs.cmd
@@ -1,4 +1,4 @@
 @echo off
 REM This file requires Debugging Tools for Windows, Perl (e.g., ActivePerl), and svn.exe (e.g., CollabNet/Tigris)
-call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\40.Rx /Debug
+call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=Nito.KitchenSink\bin\Release /Debug
 pause
