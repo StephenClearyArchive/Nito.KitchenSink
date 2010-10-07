b93507ef320517ac04ff7cd3a1d08606526d5466
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-10-07 20:57:10 +0000

    Added simple parsing classes and option attributes. (Currently untested)

---
+++ Source/Nito.KitchenSink/Options/PositionalArgumentsAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink.Options
+{
+    /// <summary>
+    /// Specifies that the command-line sets this property to the remaining positional arguments. The property type must be a collection.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public sealed class PositionalArgumentsAttribute : Attribute
+    {
+    }
+}
---
+++ Source/Nito.KitchenSink/Text/SimpleParserAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink.Text
+{
+    /// <summary>
+    /// Specifies that a given parser should be used for this type or member.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Property)]
+    public sealed class SimpleParserAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleParserAttribute"/> class.
+        /// </summary>
+        /// <param name="parserType">Type of the parser.</param>
+        public SimpleParserAttribute(Type parserType)
+        {
+            if (parserType.GetInterface("Nito.KitchenSink.Text.ISimpleParser") == null)
+            {
+                throw new InvalidOperationException("The type passed to SimpleParserAttribute must implement ISimpleParser.");
+            }
+
+            this.ParserType = parserType;
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the parser.
+        /// </summary>
+        /// <value>The type of the parser.</value>
+        public Type ParserType { get; set; }
+    }
+}
