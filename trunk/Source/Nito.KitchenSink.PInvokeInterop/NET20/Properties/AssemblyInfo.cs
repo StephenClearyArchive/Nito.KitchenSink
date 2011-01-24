657e82688dbfb082497871aacf79d4dbda0caad2
shammah_cp <SND\shammah_cp>
2011-01-24 18:42:57 +0000

    Added NuGet package for PInvokeInterop.

---
+++ trunk/Source/Nito.KitchenSink.PInvokeInterop/NET20/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Nito Programs">
+//     Copyright (c) 2009-2011 Nito Programs.
+// </copyright>
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("P/Invoke Interop Helper Library for .NET Framework 2.0")]
