3cddc5e7d12c63392ad62935044365d15dd320f0
shammah_cp <SND\shammah_cp>
2011-01-25 15:27:44 +0000

    Added NuGet package for GetPropertyName.

---
+++ trunk/Source/Nito.KitchenSink.GetPropertyName/NET35/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Nito Programs">
+//     Copyright (c) 2009-2011 Nito Programs.
+// </copyright>
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("GetPropertyName support for .NET Framework 3.5 SP1 Client Profile")]
