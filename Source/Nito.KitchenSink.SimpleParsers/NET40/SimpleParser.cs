9a6a9fb5931a65254575e4e2b35bde8c3ab23713
Stephen Cleary <StephenCleary@users.noreply.github.com>
2011-01-25 21:56:04 +0000

    Added NuGet package for SimpleParsers.

---
+++ Source/Nito.KitchenSink.SimpleParsers/NET40/SimpleParser.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="SimpleParser.cs" company="Nito Programs">
+//     Copyright (c) 2010-2011 Nito Programs.
+// </copyright>
+
+namespace Nito.KitchenSink.SimpleParsers
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// A simple parser for a specific type.
+    /// </summary>
+    [ContractClass(typeof(SimpleParserContracts))]
+    public interface ISimpleParser
+    {
+        /// <summary>
+        /// The type of the result of the <see cref="TryParse"/> method. This cannot be the <see cref="System.String"/> type or a nullable value type.
+        /// </summary>
+        Type ResultType { get; }
+
+        /// <summary>
+        /// Attempts to parse a string value into a value of type <see cref="ResultType"/>.
+        /// </summary>
+        /// <param name="value">The string value to parse.</param>
+        /// <returns>The resulting parsed value, or <c>null</c> if parsing was not possible.</returns>
+        object TryParse(string value);
+    }
+
+    [ContractClassFor(typeof(ISimpleParser))]
+    internal abstract class SimpleParserContracts : ISimpleParser
+    {
+        public Type ResultType
+        {
+            [SuppressMessage("Microsoft.Contracts", "CC1036", Justification = "Nullable.GetUnderlyingType is pure.")]
+            get
+            {
+                Contract.Ensures(Contract.Result<Type>() != null);
+                Contract.Ensures(Contract.Result<Type>() != typeof(string));
+                Contract.Ensures(Nullable.GetUnderlyingType(Contract.Result<Type>()) == null);
+                return null;
+            }
+        }
+
+        public object TryParse(string value)
+        {
+            Contract.Requires(value != null);
+            Contract.Ensures(Contract.Result<object>() == null || Contract.Result<object>().GetType() == this.ResultType);
+            return null;
+        }
+    }
+}
---
+++ Source/Nito.KitchenSink.SimpleParsers/SimpleParserAttribute.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="SimpleParserAttribute.cs" company="Nito Programs">
+//     Copyright (c) 2010-2011 Nito Programs.
+// </copyright>
+
+namespace Nito.KitchenSink.SimpleParsers
+{
+    using System;
+    using System.Diagnostics.Contracts;
+
+    /// <summary>
+    /// Specifies that a given parser should be used for this type or member.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Property)]
+    public sealed class SimpleParserAttribute : Attribute
+    {
+        private Type parserType;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleParserAttribute"/> class.
+        /// </summary>
+        /// <param name="parserType">Type of the parser.</param>
+        public SimpleParserAttribute(Type parserType)
+        {
+            Contract.Requires(parserType != null);
+            Contract.Requires(parserType.GetInterface("Nito.KitchenSink.SimpleParsers.ISimpleParser") != null, "The type passed to SimpleParserAttribute must implement ISimpleParser.");
+
+            this.parserType = parserType;
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the parser.
+        /// </summary>
+        /// <value>The type of the parser.</value>
+        public Type ParserType
+        {
+            get
+            {
+                Contract.Ensures(Contract.Result<Type>() != null);
+                Contract.Ensures(Contract.Result<Type>().GetInterface("Nito.KitchenSink.SimpleParsers.ISimpleParser") != null);
+
+                return this.parserType;
+            }
+            
+            set
+            {
+                Contract.Requires(value != null);
+                Contract.Requires(value.GetInterface("Nito.KitchenSink.SimpleParsers.ISimpleParser") != null, "The type passed to SimpleParserAttribute must implement ISimpleParser.");
+
+                this.parserType = value;
+            }
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.parserType != null);
+            Contract.Invariant(this.parserType.GetInterface("Nito.KitchenSink.SimpleParsers.ISimpleParser") != null);
+        }
+    }
+}
