a27c0f506b9cbd40a24ddba68030cf65381a1833
shammah_cp <SND\shammah_cp>
2011-01-27 20:24:04 +0000

    Added NuGet package for NotifyPropertyChanged.

---
+++ trunk/Source/Nito.KitchenSink.NotifyPropertyChanged/NotifyPropertyChangedBase.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="NotifyPropertyChangedBase.cs" company="Nito Programs">
+//     Copyright (c) 2009-2011 Nito Programs.
+// </copyright>
+
+namespace Nito.KitchenSink.NotifyPropertyChanged
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+    using System.Diagnostics.Contracts;
+
+    /// <summary>
+    /// A base class for classes that need to implement <see cref="INotifyPropertyChanged"/>.
+    /// </summary>
+    /// <typeparam name="TObject">The type of the derived class.</typeparam>
+    public abstract class NotifyPropertyChangedBase<TObject> : INotifyPropertyChanged
+        where TObject : NotifyPropertyChangedBase<TObject>
+    {
+        /// <summary>
+        /// The backing delegate for <see cref="PropertyChanged"/>.
+        /// </summary>
+        private PropertyChangedEventHandler propertyChanged;
+
+        /// <summary>
+        /// Provides notification of changes to a property value.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { this.propertyChanged += value; }
+            remove { this.propertyChanged -= value; }
+        }
+
+        /// <summary>
+        /// Raises <see cref="PropertyChanged"/> for the Items[] property.
+        /// </summary>
+        protected void OnItemsPropertyChanged()
+        {
+            this.propertyChanged.RaiseItems(this);
+        }
+
+        /// <summary>
+        /// Raises <see cref="PropertyChanged"/> for the given property.
+        /// </summary>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <param name="expression">The lambda expression identifying the property that changed. May not be <c>null</c>.</param>
+        protected void OnPropertyChanged<TProperty>(Expression<Func<TObject, TProperty>> expression)
+        {
+            Contract.Requires(expression != null);
+
+            // The cast of "this" to TObject will always succeed due to the generic constraint on this class
+            this.propertyChanged.Raise((TObject)this, expression);
+        }
+
+        /// <summary>
+        /// Raises <see cref="PropertyChanged"/> for all properties.
+        /// </summary>
+        protected void OnPropertyChanged()
+        {
+            this.propertyChanged.Raise(this);
+        }
+    }
+}
---
+++ trunk/Source/Nito.KitchenSink.NotifyPropertyChanged/PropertyChangedEventHandlerExtensions.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="PropertyChangedEventHandlerExtensions.cs" company="Nito Programs">
+//     Copyright (c) 2009-2011 Nito Programs.
+// </copyright>
+
+namespace Nito.KitchenSink.NotifyPropertyChanged
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+    using Nito.KitchenSink.GetPropertyName;
+    using System.Diagnostics.Contracts;
+
+    /// <summary>
+    /// Provides extension methods for <see cref="PropertyChangedEventHandler"/> delegates.
+    /// </summary>
+    public static class PropertyChangedEventHandlerExtensions
+    {
+        /// <summary>
+        /// Raises the delegate for the property identified by a lambda expression.
+        /// </summary>
+        /// <typeparam name="TObject">The type of object containing the property.</typeparam>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <param name="handler">The delegate to raise. If this parameter is null, then no action is taken.</param>
+        /// <param name="sender">The object raising this event.</param>
+        /// <param name="expression">The lambda expression identifying the property that changed. May not be <c>null</c>.</param>
+        public static void Raise<TObject, TProperty>(
+            this PropertyChangedEventHandler handler,
+            TObject sender,
+            Expression<Func<TObject, TProperty>> expression)
+        {
+            Contract.Requires(expression != null);
+
+            if (handler != null)
+            {
+                handler(sender, new PropertyChangedEventArgs(sender.GetPropertyName(expression)));
+            }
+        }
+
+        /// <summary>
+        /// Raises the delegate for the items property (with the name "Items[]").
+        /// </summary>
+        /// <param name="handler">The delegate to raise. If this parameter is null, then no action is taken.</param>
+        /// <param name="sender">The object raising this event.</param>
+        public static void RaiseItems(this PropertyChangedEventHandler handler, object sender)
+        {
+            if (handler != null)
+            {
+                handler(sender, new PropertyChangedEventArgs("Items[]"));
+            }
+        }
+
+        /// <summary>
+        /// Raises the delegate for all properties (with an empty string).
+        /// </summary>
+        /// <param name="handler">The delegate to raise. If this parameter is null, then no action is taken.</param>
+        /// <param name="sender">The object raising this event.</param>
+        public static void Raise(this PropertyChangedEventHandler handler, object sender)
+        {
+            if (handler != null)
+            {
+                handler(sender, new PropertyChangedEventArgs(string.Empty));
+            }
+        }
+    }
+}
