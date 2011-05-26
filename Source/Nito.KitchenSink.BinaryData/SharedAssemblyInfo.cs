af185bdbdc2b30fbec8190740f2296f21061befa
Stephen Cleary <StephenCleary@users.noreply.github.com>
2011-05-26 04:22:12 +0000

    Updated all NuGet packages to NuGet-style source packages, except FileSystemPath.

--- Source/Nito.KitchenSink.BinaryData/SharedAssemblyInfo.cs
+++ Source/Nito.KitchenSink.BinaryData/SharedAssemblyInfo.cs
@@ -6,11 +6,11 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("Binary Data Helper Library")]
+[assembly: AssemblyDescription("A helper library for working with byte sequences and arrays.")]
 [assembly: AssemblyCompany("Nito Programs")]
 [assembly: AssemblyProduct("Nito.KitchenSink Library")]
 [assembly: AssemblyCopyright("Copyright © 2009-2011 Nito Programs")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
