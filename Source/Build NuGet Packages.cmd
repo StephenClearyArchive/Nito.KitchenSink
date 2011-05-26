25b1396d7cb1c635cd186e1134656aaae3ad4d13
Stephen Cleary <StephenCleary@users.noreply.github.com>
2011-05-26 05:30:38 +0000

    Updated FileSystemPaths to use a NuGet-style source package.

--- Source/Build NuGet Packages.cmd
+++ Source/Build NuGet Packages.cmd
@@ -2,7 +2,6 @@
 if not exist ..\Binaries mkdir ..\Binaries
 if not exist ..\Binaries\NuGet mkdir ..\Binaries\NuGet
 ..\Util\nuget.exe pack -sym Nito.KitchenSink.PInvokeInterop\NET20\NET20.csproj -o ..\Binaries\NuGet
-..\Util\nuget.exe pack Nito.KitchenSink.FileSystemPath\Nito.KitchenSink.FileSystemPaths.nuspec -o ..\Binaries\NuGet
 ..\Util\nuget.exe pack -sym Nito.KitchenSink.GuidDecoding\NET35\NET35.csproj -o ..\Binaries\NuGet
 ..\Util\nuget.exe pack -sym Nito.KitchenSink.HashAlgorithms\NET35\NET35.csproj -o ..\Binaries\NuGet
 ..\Util\nuget.exe pack -sym Nito.KitchenSink.GetPropertyName\NET35\NET35.csproj -o ..\Binaries\NuGet
@@ -13,4 +12,8 @@ if not exist ..\Binaries\NuGet mkdir ..\Binaries\NuGet
 ..\Util\nuget.exe pack -sym Nito.KitchenSink.BinaryData\NET40\NET40.csproj -o ..\Binaries\NuGet
 ..\Util\nuget.exe pack -sym Nito.KitchenSink.Exceptions\NET40\NET40.csproj -o ..\Binaries\NuGet
 ..\Util\nuget.exe pack -sym Nito.KitchenSink.NotifyPropertyChanged\NET35\NET35.csproj -o ..\Binaries\NuGet
+..\Util\nuget.exe pack Nito.KitchenSink.FileSystemPath\Nito.KitchenSink.FileSystemPaths.nuspec -o ..\Binaries\NuGet
+@echo Rename Nito.KitchenSink.FileSystemPaths.*.nupkg to Nito.KitchenSink.FileSystemPaths.*.symbols.nupkg
+pause
+..\Util\nuget.exe pack Nito.KitchenSink.FileSystemPath\Nito.KitchenSink.FileSystemPaths.nuspec -o ..\Binaries\NuGet -Exclude **\*.pdb -Exclude **\*.cs
 pause
