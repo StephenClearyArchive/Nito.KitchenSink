ba85278c257648e368dee4e9200488ffabfba826
Stephen Cleary <StephenCleary@users.noreply.github.com>
2011-10-10 21:14:06 +0000

    Added framework for unit tests.

---
+++ Source/Nito.KitchenSink.OptionParsing/UnitTests/ConsoleCommandLineLexerUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nito.KitchenSink.OptionParsing;
+using System.Diagnostics;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ConsoleCommandLineLexerUnitTests
+    {
+        [DebuggerStepThrough]
+        private void AssertSequenceEqual<T>(IEnumerable<T> actual, IEnumerable<T> expected)
+        {
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+
+        [TestMethod]
+        public void RegularParameter()
+        {
+            AssertSequenceEqual(new[] { "a" }, ConsoleCommandLineLexer.Lex("a"));
+        }
+    }
+}
