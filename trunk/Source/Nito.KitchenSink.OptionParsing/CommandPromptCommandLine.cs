9920be12fbc6d72a2cea3758a3a4cb3f2ed6adef
shammah_cp <SND\shammah_cp>
2011-10-10 19:18:36 +0000

    Added ConsoleCommandLineLexer, and made NitoCommandLineLexer the default. This is a breaking change.

---
+++ trunk/Source/Nito.KitchenSink.OptionParsing/CommandPromptCommandLine.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="CommandPromptCommandLine.cs" company="Nito Programs">
+//     Copyright (c) 2011 Nito Programs.
+// </copyright>
+
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Nito.KitchenSink.OptionParsing
+{
+    /// <summary>
+    /// Provides command-prompt-compatible escaping for command lines.
+    /// </summary>
+    public static class CommandPromptCommandLine
+    {
+        /// <summary>
+        /// Takes a command line to pass to a program, and escapes any unquoted characters that are special to the command prompt shell: <c>&amp; | ( ) &lt; &gt; ^</c>
+        /// </summary>
+        /// <param name="commandLine">The command line to pass to the program.</param>
+        /// <returns>The command line to send to the command prompt shell.</returns>
+        public static string Escape(string commandLine)
+        {
+            Contract.Requires(commandLine != null);
+            Contract.Ensures(Contract.Result<string>() != null);
+
+            var ret = new StringBuilder();
+            bool inQuote = false;
+            foreach (var ch in commandLine)
+            {
+                if (ch == '"')
+                {
+                    // Quote characters begin or end a quoted context, and are always passed through.
+                    inQuote = !inQuote;
+                    ret.Append(ch);
+                }
+                else if (inQuote)
+                {
+                    // If we're in a quoted context, no special characters need to be escaped.
+                    ret.Append(ch);
+                }
+                else if (ch == '&' || ch == '|' || ch == '(' || ch == ')' || ch == '<' || ch == '>' || ch == '^')
+                {
+                    // Special characters outside a quoted context need to be escaped.
+                    ret.Append('^');
+                    ret.Append(ch);
+                }
+                else
+                {
+                    // Regular characters outside a quoted context do not need to be escaped.
+                    ret.Append(ch);
+                }
+            }
+
+            return ret.ToString();
+        }
+    }
+}
---
+++ trunk/Source/Nito.KitchenSink.OptionParsing/ConsoleCommandLineLexer.cs
@@ -0,0 +1,172 @@
+﻿// <copyright file="ConsoleCommandLineLexer.cs" company="Nito Programs">
+//     Copyright (c) 2011 Nito Programs.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace Nito.KitchenSink.OptionParsing
+{
+    /// <summary>
+    /// Provides the default Win32 Console command-line lexing.
+    /// </summary>
+    public static class ConsoleCommandLineLexer
+    {
+        private enum LexerState
+        {
+            /// <summary>
+            /// The default state; no data exists in the argument character buffer.
+            /// </summary>
+            Default,
+
+            /// <summary>
+            /// An argument has been started.
+            /// </summary>
+            Argument,
+
+            /// <summary>
+            /// A quote character has been seen, and we are now parsing quoted data.
+            /// </summary>
+            Quoted,
+        }
+
+        /// <summary>
+        /// Lexes the command line, using the same rules as CommandLineToArgvW.
+        /// </summary>
+        /// <param name="commandLine">The command line to parse.</param>
+        /// <returns>The lexed command line.</returns>
+        public static IEnumerable<string> Lex(this string commandLine)
+        {
+            Contract.Requires(commandLine != null);
+            Contract.Ensures(Contract.Result<IEnumerable<string>>() != null);
+
+            LexerState state = LexerState.Default;
+            int backslashes = 0;
+
+            string result = string.Empty;
+            foreach (var ch in commandLine)
+            {
+                if (ch == '"')
+                {
+                    // Handle the special rules for any backslashes preceding a double-quote.
+                    result += new string('\\', backslashes / 2);
+                    if ((backslashes % 2) == 0)
+                    {
+                        // An even number of backslashes means that this is a normal double-quote.
+                        if (state == LexerState.Quoted)
+                            state = LexerState.Argument;
+                        else
+                            state = LexerState.Quoted;
+                    }
+                    else
+                    {
+                        // An odd number of backslashes means the double-quote is escaped.
+                        result += '"';
+                        Contract.Assume(state != LexerState.Default);
+                    }
+
+                    backslashes = 0;
+                }
+                else if (ch == '\\')
+                {
+                    ++backslashes;
+                    if (state == LexerState.Default)
+                        state = LexerState.Argument;
+                }
+                else
+                {
+                    // Consume any backslashes preceding the whitespace or normal character.
+                    result += new string('\\', backslashes);
+                    backslashes = 0;
+
+                    // CommandLineToArgvW only recognizes spaces and tabs as whitespace.
+                    if (ch == ' ' || ch == '\t')
+                    {
+                        if (state == LexerState.Quoted)
+                        {
+                            // Quoted whitespace characters are just added to the token.
+                            result += ch;
+                        }
+                        else if (state == LexerState.Argument)
+                        {
+                            // Unquoted whitespace characters separate tokens.
+                            yield return result;
+                            result = string.Empty;
+                            state = LexerState.Default;
+                        }
+                    }
+                }
+            }
+
+            result += new string('\\', backslashes);
+
+            if (state != LexerState.Default)
+            {
+                yield return result;
+            }
+        }
+
+        /// <summary>
+        /// Lexes the command line for this process, using the same rules as CommandLineToArgvW.
+        /// </summary>
+        /// <returns>The lexed command line.</returns>
+        public static IEnumerable<string> Lex()
+        {
+            return Environment.GetCommandLineArgs().Skip(1);
+        }
+
+        /// <summary>
+        /// Takes a list of arguments to pass to a program, and quotes them. This method does not quote or escape special shell characters (see <see cref="CommandPromptCommandLine"/>).
+        /// </summary>
+        /// <param name="arguments">The arguments to quote (if necessary) and concatenate into a command line.</param>
+        /// <returns>The command line.</returns>
+        public static string Escape(IEnumerable<string> arguments)
+        {
+            Contract.Requires(arguments != null);
+            Contract.Ensures(Contract.Result<string>() != null);
+
+            // Escape each argument (if necessary) and join them with spaces.
+            return string.Join(" ", arguments.Select(argument =>
+            {
+                Contract.Assume(argument != null);
+
+                // An argument does not need escaping if it does not have any whitespace or quote characters.
+                if (!argument.Any(ch => ch == ' ' || ch == '\t' || ch == '"') && argument != string.Empty)
+                {
+                    return argument;
+                }
+
+                // To escape the argument, wrap it in double-quotes and escape existing double-quotes, doubling any existing escape characters but only if they precede a double-quote.
+                var ret = new StringBuilder();
+                ret.Append('"');
+                int backslashes = 0;
+                foreach (var ch in argument)
+                {
+                    if (ch == '\\')
+                    {
+                        ++backslashes;
+                    }
+                    else if (ch == '"')
+                    {
+                        ret.Append(new string('\\', 2 * backslashes + 1));
+                        backslashes = 0;
+                        ret.Append(ch);
+                    }
+                    else
+                    {
+                        ret.Append(new string('\\', backslashes));
+                        backslashes = 0;
+                        ret.Append(ch);
+                    }
+                }
+
+                ret.Append(new string('\\', backslashes));
+                ret.Append('"');
+                return ret.ToString();
+            }));
+        }
+    }
+}
