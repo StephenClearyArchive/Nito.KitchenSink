d3d2d97064677191ced05d3331c9228d23b9dfdf
shammah_cp <SND\shammah_cp>
2011-10-11 15:40:32 +0000

    Corrected unit tests for CommandPromptCommandLine.

--- trunk/Source/Nito.KitchenSink.OptionParsing/UnitTests/CommandPromptCommandLineUnitTests.cs
+++ trunk/Source/Nito.KitchenSink.OptionParsing/UnitTests/CommandPromptCommandLineUnitTests.cs
@@ -11,54 +11,25 @@ namespace UnitTests
     [TestClass]
     public class CommandPromptCommandLineUnitTests
     {
-        [DebuggerStepThrough]
-        private void AssertSequenceEqual<T>(IEnumerable<T> expected, IEnumerable<T> actual)
-        {
-            Assert.IsTrue(actual.SequenceEqual(expected), "Expected: [" + string.Join(", ", expected) + "]; actual: [" + string.Join(", ", actual) + "]");
-        }
-
-        private const string Quote = "\"";
-
         [TestMethod]
         public void BlogExampleA()
         {
             // http://nitoprograms.blogspot.com/2011/06/option-parsing-lexing.html
-            AssertSequenceEqual(new[] { "^", Quote + "^" + Quote }, ConsoleCommandLineLexer.Lex("^^ " + Quote + "^" + Quote));
+            Assert.AreEqual("^^ \"^\"", CommandPromptCommandLine.Escape("^ \"^\""));
         }
 
         [TestMethod]
         public void BlogExampleB()
         {
             // http://nitoprograms.blogspot.com/2011/06/option-parsing-lexing.html
-            AssertSequenceEqual(new[] { Quote + Quote }, ConsoleCommandLineLexer.Lex("^" + Quote + "^" + Quote));
+            Assert.AreEqual("\"\"", CommandPromptCommandLine.Escape("\"\""));
         }
 
         [TestMethod]
         public void BlogExampleC()
         {
             // http://nitoprograms.blogspot.com/2011/06/option-parsing-lexing.html
-            AssertSequenceEqual(new[] { Quote + Quote }, ConsoleCommandLineLexer.Lex("^" + Quote + Quote));
-        }
-
-        [TestMethod]
-        public void BlogExampleD()
-        {
-            // http://nitoprograms.blogspot.com/2011/06/option-parsing-lexing.html
-            AssertSequenceEqual(new[] { Quote + "^" + Quote + Quote }, ConsoleCommandLineLexer.Lex(Quote + "^" + Quote + "^" + Quote));
-        }
-
-        [TestMethod]
-        public void BlogExampleE()
-        {
-            // http://nitoprograms.blogspot.com/2011/06/option-parsing-lexing.html
-            AssertSequenceEqual(new[] { Quote + "^" + Quote + "^" + Quote }, ConsoleCommandLineLexer.Lex(Quote + "^" + Quote + "^^" + Quote));
-        }
-
-        [TestMethod]
-        public void BlogExampleF()
-        {
-            // http://nitoprograms.blogspot.com/2011/06/option-parsing-lexing.html
-            AssertSequenceEqual(new[] { Quote + "^^" }, ConsoleCommandLineLexer.Lex(Quote + "^^"));
+            Assert.AreEqual("\"^\"^^\"", CommandPromptCommandLine.Escape("\"^\"^\""));
         }
     }
 }
